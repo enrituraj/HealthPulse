82097b757d86baeb4e2d996cb914d6f770ae48ae
Ritu <rn26rituraj@gmail.com>
2023-11-02 07:23:11 +0530

    testing in  production

--- api/index.py
+++ api/index.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return 'About'
+if __name__ == '__main__':
+    app.run(debug=True)
+
